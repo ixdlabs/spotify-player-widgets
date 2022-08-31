537bbcb105ae1087b15653513bfdb506e8758175
binoy <binoy.18@cse.mrt.ac.lk>
2022-08-31 19:26:28 +0530

    Icon not loading bug fixed

--- lib/src/constants/asset_constants.dart
+++ lib/src/constants/asset_constants.dart
@@ -1,3 +1,4 @@
 abstract class AssetConstants {
-  static const String kSpotifyLogo = "assets/spotify_icon.svg";
+  static const String kSpotifyLogo =
+      "packages/spotify_player_widgets/lib/assets/spotify_icon.svg";
 }
